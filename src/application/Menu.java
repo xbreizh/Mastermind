4a6b1b2aa8208b0abef35eaf04dadb4df7c75ba7
xbreizh <xavier.lamourec@gmail.com>
2018-03-02 15:21:03 +0100

    created new Menu

---
+++ src/application/Menu.java
@@ -0,0 +1,60 @@
+package application;
+
+import controller.Controller;
+import controller.Mode;
+import controller.Status;
+import game.Games;
+
+
+public class Menu {
+	Controller ct;
+	public Menu(){
+		
+	}
+	
+	public Games checkGame() {
+		int choice;
+		do {
+			sendOutput(Status.GAME_MENU.getOutput());
+			choice = askForInput();
+		} while (choice!=1 && choice!=2);
+		if (choice==1) {
+			return Games.MoreLess;
+		}
+		else {
+			return Games.MasterMind;
+		}
+
+	}
+	public Mode checkMode() {
+		int choice;
+		do {
+			sendOutput(Status.MODE_MENU.getOutput());
+			choice = askForInput();
+		} while (choice!=1 && choice!=2 && choice!=3);
+		if (choice==1) {
+			return Mode.CHALLENGER;
+		}
+		if (choice==2) {
+			return Mode.DEFENSER;
+		}
+		else {
+			return Mode.DUAL;
+		}
+
+	}
+	int askForInput() {
+		int input = ct.getInput();
+		return input;
+	}
+	
+	void sendOutput(String output) {
+		ct.sendOutput(output);
+	}
+
+	public void setController(Controller ct) {
+		this.ct = ct;
+	}
+
+
+}
